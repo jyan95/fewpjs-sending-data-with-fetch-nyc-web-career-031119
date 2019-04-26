8c5e68e0df86e173227481e78b7341cfe2663c80
github username <github email address>
2019-04-26 11:50:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(username, email) {
+  return fetch('http://localhost:3000/users',{
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      name: `${username}`,
+      email: `${email}`
+    }
+    )
+  })
+  .then(res => res.json())
+  .then(json => renderInfo(json))
+  .catch(err => renderError(err.message));
+}
+
+function renderInfo(json) {
+  document.querySelector('body').innerHTML = `${json.id}`
+}
+
+function renderError(err) {
+  document.querySelector('body').innerHTML = `${err}`
+}
